b423b180933f7021bf207396ab60b3010efa714e
Arthur Clemens <arthurclemens@gmail.com>
2015-11-20 11:45:40 +0100

    New icons from upstream updates

---
+++ templarian/msvg/stairs.js
@@ -0,0 +1,2 @@
+var m = require('mithril');
+module.exports = m.trust('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" baseProfile="full" width="24" height="24" viewBox="0 0 24.00 24.00" enable-background="new 0 0 24.00 24.00" xml:space="preserve"><path fill="#000000" fill-opacity="1" stroke-width="1.33333" stroke-linejoin="miter" d="M 15,5L 15,9L 11,9L 11,13L 7,13L 7,17L 3,17L 3,20L 10,20L 10,16L 14,16L 14,12L 18,12L 18,8L 22,8L 22,5L 15,5 Z "/></svg>');
